58bc31ea914d5fec7f469c993c322762a448e6e1
lraffin <louis.raffin.pro@gmail.com>
2024-10-14 20:45:19 +0200

    Déclaration de fonctions d'affichage et création de la fonction afficherBrochette

--- src/Affichage.cpp
+++ src/Affichage.cpp
@@ -1,1 +1,57 @@
 #include "Affichage.h"
+#include "Plateau.h"
+
+
+//Affichage des dialogues
+/*
+void afficherInstructions()
+{
+
+}
+
+void afficherOptionsDeJeu()
+{
+    
+}
+
+void afficherTourJoueur(Joueur joueur)
+{
+
+}
+
+void afficherScores()
+{
+
+}
+
+void afficherVainqueur()
+{
+
+}
+*/
+
+//Affichage du plateau
+/*
+void afficherPileJoueurEnCours()
+{
+
+}
+
+void afficherDes()
+{
+
+}
+*/
+void afficherBrochette(Pickomino brochette[NB_PICKOMINO]){
+    for (int i = 0; i < NB_PICKOMINO; ++i)
+    {
+        if (brochette[i].etat == 0)
+        {
+            std::cout << brochette[i].valeur 
+                      << brochette[i].nombreDeVers
+                      << std::endl;
+        }
+        else if (brochette[i].etat == 1)
+            std::cout << 'X';
+    }
+}
--- src/Affichage.h
+++ src/Affichage.h
@@ -1,4 +1,22 @@
 #ifndef AFFICHAGE_H
 #define AFFICHAGE_H
 
+#include "Donnees.h"
+
+#include <iostream>
+
+/*
+void afficherInstructions();
+void afficherOptionsDeJeu();
+void afficherTourJoueur(Joueur joueur);
+void afficherScores();
+void afficherVainqueur();
+*/
+
+/*
+void afficherPileJoueurEnCours()
+void afficherDes()
+*/
+void afficherBrochette(Pickomino brochette[NB_PICKOMINO]);
+
 #endif
